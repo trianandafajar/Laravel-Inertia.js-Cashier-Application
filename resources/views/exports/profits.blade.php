282b7ede9c33373fadc0ca75cde1580aa1b053f0
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-15 11:24:12 +0700

    Update Profits

--- resources/views/exports/profits.blade.php
+++ resources/views/exports/profits.blade.php
@@ -1,15 +1,16 @@
-<div class="title" style="padding-bottom: 13px">
-    <div style="text-align: center;text-transform: uppercase;font-size: 15px">
-        Yan Afriyoko
+<div class="title" style="padding-bottom: 13px;">
+    <div style="text-align: center; text-transform: uppercase; font-size: 15px;">
+        Triananda Fajar Ramadhan
     </div>
-    <div style="text-align: center">
-        Alamat: Desa Gedangalas, Kec. Gajah, Kab. Demak
+    <div style="text-align: center;">
+        Alamat: Desa Kedungombo, Kec. Tengaran, Kab. Semarang
     </div>
-    <div style="text-align: center">
+    <div style="text-align: center;">
         Telp: 0857-9087-9087
     </div>
 </div>
-<table style="width: 100%">
+
+<table style="width: 100%;">
     <thead>
         <tr style="background-color: #e6e6e7;">
             <th scope="col">Date</th>
@@ -25,9 +26,9 @@
             <td class="text-end">{{ formatPrice($profit->total) }}</td>
         </tr>
         @endforeach
-        <tr>
-            <td colspan="2" class="text-end fw-bold" style="background-color: #e6e6e7;">TOTAL</td>
-            <td class="text-end fw-bold" style="background-color: #e6e6e7;">{{ formatPrice($total) }}</td>
+        <tr style="background-color: #e6e6e7;">
+            <td colspan="2" class="text-end fw-bold">TOTAL</td>
+            <td class="text-end fw-bold">{{ formatPrice($total) }}</td>
         </tr>
     </tbody>
 </table>
