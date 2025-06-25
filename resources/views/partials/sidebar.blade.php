6eedc49a57d90334485695a992e3772923868487
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-25 09:54:01 +0700

    Add Sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,31 @@
+<nav class="d-none d-md-block bg-light sidebar col-md-2">
+  <div class="position-sticky pt-3">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" href="/apps/dashboard">
+          <i class="fas fa-tachometer-alt"></i> Dashboard
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/apps/products">
+          <i class="fas fa-box"></i> Produk
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/apps/transactions">
+          <i class="fas fa-cash-register"></i> Transaksi
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/apps/sales">
+          <i class="fas fa-chart-line"></i> Penjualan
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/apps/profits">
+          <i class="fas fa-coins"></i> Profit
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav> 
